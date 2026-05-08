0d054c278ab317f3b0c78500f40c1cf9b6d10e9f
Cheong Hoi Chun <127018012+Yeet94@users.noreply.github.com>
2026-05-08 16:24:07 +0800

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/8/2026, 4:24:07 PM
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hash;
+        for(int i =0; i<nums.size(); i++){
+            hash[nums[i]] = i;
+        }
+        for(int i =0; i<nums.size(); i++){
+            int complement = target - nums[i];
+
+            if(hash.find(complement) != hash.end() && hash[complement] != i){
+                return{i, hash[complement]};
+            }
+        }
+        return {};
+    }
+};
