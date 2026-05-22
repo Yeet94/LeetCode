281f9784a55ab33e1fb08494de0c591507a42b22
Cheong Hoi Chun <127018012+Yeet94@users.noreply.github.com>
2026-05-22 21:09:43 +0800

    Create 724-FindPivotIndex

---
+++ 724-FindPivotIndex/version/python/724-FindPivotIndex_20260522_210941.py
@@ -0,0 +1,8 @@
+# Last updated: 5/22/2026, 9:09:41 PM
+1class Solution:
+2    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+3        set1 = set(nums1)
+4        set2 = set(nums2)
+5
+6        return [list(set1-set2),list(set2-set1)]
+7
