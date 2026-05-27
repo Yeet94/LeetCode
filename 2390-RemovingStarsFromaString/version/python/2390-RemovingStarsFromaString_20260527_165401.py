5651b7e71872a955c19dc85a46f58b0eac0f1dfd
Cheong Hoi Chun <127018012+Yeet94@users.noreply.github.com>
2026-05-27 16:54:01 +0800

    Create 2390-RemovingStarsFromaString

---
+++ 2390-RemovingStarsFromaString/version/python/2390-RemovingStarsFromaString_20260527_165401.py
@@ -0,0 +1,14 @@
+# Last updated: 5/27/2026, 4:54:01 PM
+1class Solution:
+2    def removeStars(self, s: str) -> str:
+3        s_list = list(s)
+4        write = 0
+5
+6        for read in range(len(s_list)):
+7            if s_list[read] == "*":
+8                write -= 1
+9            else:
+10                s_list[write] = s_list[read]
+11                write += 1
+12            
+13        return "".join(s_list[:write])
