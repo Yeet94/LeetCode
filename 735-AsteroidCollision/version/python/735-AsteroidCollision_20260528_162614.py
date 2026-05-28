0de316107c852bcbbf28df2d33a608e0c1f736be
Cheong Hoi Chun <127018012+Yeet94@users.noreply.github.com>
2026-05-28 16:26:14 +0800

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/version/python/735-AsteroidCollision_20260528_162614.py
@@ -0,0 +1,21 @@
+# Last updated: 5/28/2026, 4:26:14 PM
+1class Solution:
+2    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+3        res = []
+4        for i in (asteroids):
+5            alive = True
+6            while res and i < 0 and res[-1] > 0:
+7                if abs(i) > res[-1]:
+8                    res.pop()
+9                elif abs(i) == res[-1]:
+10                    res.pop()
+11                    alive = False
+12                    break
+13                else:
+14                    alive = False
+15                    break
+16
+17            if alive:
+18                res.append(i)
+19        
+20        return res
