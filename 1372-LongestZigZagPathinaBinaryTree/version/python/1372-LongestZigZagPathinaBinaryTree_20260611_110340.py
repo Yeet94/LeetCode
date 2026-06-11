5f621a18535eb572ff6e4210acfbcf0322d6ca18
Cheong Hoi Chun <127018012+Yeet94@users.noreply.github.com>
2026-06-11 11:03:42 +0800

    Create 1372-LongestZigZagPathinaBinaryTree

---
+++ 1372-LongestZigZagPathinaBinaryTree/version/python/1372-LongestZigZagPathinaBinaryTree_20260611_110340.py
@@ -0,0 +1,44 @@
+# Last updated: 6/11/2026, 11:03:40 AM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, x):
+4#         self.val = x
+5#         self.left = None
+6#         self.right = None
+7
+8class Solution:
+9    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+10        stack = [root]
+11        parent_map = {root:None}
+12        while p not in parent_map or q not in parent_map:
+13            node = stack.pop()
+14
+15            if node.left:
+16                parent_map[node.left] = node
+17                stack.append(node.left)
+18
+19            if node.right:
+20                parent_map[node.right] = node
+21                stack.append(node.right)
+22
+23        p_ancestors = []
+24        q_ancestors = []
+25
+26        while p:
+27            p_ancestors.append(p)
+28            p = parent_map[p]
+29        
+30        while q:
+31            q_ancestors.append(q)
+32            q = parent_map[q]
+33
+34        
+35        i = 0 
+36        while i < len(p_ancestors):
+37            if p_ancestors[i] in q_ancestors:
+38                return p_ancestors[i]
+39            i += 1
+40        
+41        
+42
+43                
