be5cd26ac09d2b7c602e4a48fe6fdf0bdbda91de
Cheong Hoi Chun <127018012+Yeet94@users.noreply.github.com>
2026-08-25 20:07:22 +0800

    Create 746-MinCostClimbingStairs

---
+++ 746-MinCostClimbingStairs/version/python/746-MinCostClimbingStairs_20260825_200721.py
@@ -0,0 +1,14 @@
+# Last updated: 8/25/2026, 8:07:21 PM
+1class Solution:
+2    def minCostClimbingStairs(self, cost: List[int]) -> int:
+3        first = cost[0]
+4        second = cost[1]
+5
+6        for i in range(2,len(cost)):
+7            curr = cost[i] + min(first,second)
+8            first,second = second,curr
+9        
+10        return min(first,second)
+11
+12
+13            
